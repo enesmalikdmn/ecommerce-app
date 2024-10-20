3afe5b8b4d60dfb2bffc57d6593e09cc9d08bc7c
enesmalikdmn <m.duman@digitastic.app>
2024-10-20 18:29:02 +0300

    Refactor axios request interceptor to include authorization header

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,18 @@
 import axios from 'axios'
 
+axios.interceptors.request.use(function (config) {
+    const { origin } = new URL(config.url);
+    const allowedOrigins = [process.env.REACT_APP_API_URL];
+    
+    if (allowedOrigins.includes(origin)) {
+        config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
+    }
+
+    return config;
+  }, function (error) {
+    return Promise.reject(error);
+  });
+
 export const getProducts = async ({pageParam = 0}) => {
     const { data } = await axios.get(`${process.env.REACT_APP_API_URL}/products?offset=${pageParam}&limit=${12}`)
     return data
